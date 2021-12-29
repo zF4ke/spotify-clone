7d69203b49afc9c9a2496855d40b5c9bd0d5c67a
zF4ke <pedrohsilva955@gmail.com>
2021-12-29 02:06:24 +0000

    this isnt funny

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,6 +6,8 @@ export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.JWT_SECRET })
     const { pathname } = req.nextUrl
 
+    console.log(token)
+
     // allow the request if:
     // 1) it's a request for next-auth session & provider fetching
     // 2) the token exists
