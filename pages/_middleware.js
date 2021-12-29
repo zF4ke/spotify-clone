7c741c99bbcea8ef52602f53b0e10bb8a5dfa192
zF4ke <pedrohsilva955@gmail.com>
2021-12-29 02:14:29 +0000

    cmon vercel do something

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,7 +6,10 @@ export async function middleware(req) {
     const token = await getToken({ req, secret: process.env.JWT_SECRET })
     const { pathname } = req.nextUrl
 
-    console.log(token)
+    console.log("token: " + token)
+    console.log("JWT_SECRET: " + process.env.JWT_SECRET)
+    console.log("CLIENT_SECRET: " + process.env.CLIENT_SECRET)
+    console.log("CLIENT_ID: " + process.env.CLIENT_ID)
 
     // allow the request if:
     // 1) it's a request for next-auth session & provider fetching
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -30,8 +30,8 @@ export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         SpotifyProvider({
-            clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
             authorization: LOGIN_URL
         }),
         // ...add more providers here
